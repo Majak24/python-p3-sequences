8a8b48d4b8ca3bd4adf6eb92f0cd5a301f6c783a
Majak Deng <majakbanks.deng>
2024-09-25 22:37:59 +0300

    new file

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+    
+    fib_sequence = [0]
+    if length > 1:
+        fib_sequence.append(1)
+    
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+    
+    print(fib_sequence)
+
+# Test cases
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
+print_fibonacci(10)
